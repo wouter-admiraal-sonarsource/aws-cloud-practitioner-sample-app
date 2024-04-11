93fd041bd580d50286cf0abe17e28468d575dade
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2024-04-11 16:21:57 +0200

    Add simple layout mechanism

--- lib/view.php
+++ lib/view.php
@@ -5,7 +5,15 @@ function render_view($file, $vars)
   extract($vars, EXTR_SKIP);
   ob_start();
   require_once($file);
+  $content = ob_get_contents();
+  ob_end_clean();
+
+  $vars['view_content'] = $content;
+  extract($vars, EXTR_SKIP);
+  ob_start();
+  require_once("views/layout.php");
   $html = ob_get_contents();
   ob_end_clean();
+
   return $html;
 }
